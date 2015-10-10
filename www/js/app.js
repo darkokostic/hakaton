50b62080bc9f14cbd4b056f64627a3c610c33669
vukasinovic <vukasinovic@outlook.com>
2015-10-10 18:13:08 +0200

    added routin and fixed nav-back button

--- www/js/app.js
+++ www/js/app.js
@@ -1,73 +1,58 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
       cordova.plugins.Keyboard.disableScroll(true);
-
     }
     if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
     }
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  $ionicConfigProvider.backButton.text('');
+  $ionicConfigProvider.backButton.previousTitleText(false);
+
   $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
+  .state('pocetna', {
+    url: '/pocetna',
+    templateUrl: 'templates/pocetna.html',
+    controller: 'PocetnaCtrl'
   })
 
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
+  .state('upis', {
+    url: '/upis',
+    templateUrl: 'templates/upis.html',
+    controller: 'UpisCtrl'
   })
 
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
+  .state('raspored-nastave', {
+    url: '/raspored-nastave',
+    templateUrl: 'templates/raspored-nastave.html',
+    controller: 'RasporedNastaveCtrl'
+  })
 
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
+  .state('kalendar-rada', {
+    url: '/kalendar-rada',
+    templateUrl: 'templates/kalendar-rada.html',
+    controller: 'KalendarRadaCtrl'
+
+  })
+
+  .state('kontakt', {
+    url: '/kontakt',
+    templateUrl: 'templates/kontakt.html',
+    controller: 'KontaktCtrl'
+  })
+
+  .state('o-skoli', {
+    url: '/o-skoli',
+    templateUrl: 'templates/o-skoli.html',
+    controller: 'OSkoliCtrl'
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+
+  $urlRouterProvider.otherwise('/pocetna');
 });
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,42 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller("SideBarController", function ($scope, $ionicPlatform, $ionicSideMenuDelegate, $location) {
 
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    $scope.toggleLeft = function () {
 
-  // Form data for the login modal
-  $scope.loginData = {};
+        $ionicSideMenuDelegate.toggleLeft();
+    }
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
+    $scope.toggleRight = function() {
+      $ionicSideMenuDelegate.toggleRight();
+    }
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
+.controller('PocetnaCtrl', function($scope) {
+  //Pocetna controller
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+.controller('OSkoliCtrl', function($scope, $stateParams, $ionicSideMenuDelegate) {
+  $ionicSideMenuDelegate.toggleLeft();
+  //O skoli controller
+})
+
+.controller('UpisCtrl', function($scope, $ionicSideMenuDelegate) {
+  $ionicSideMenuDelegate.toggleLeft();
+  //Upis controller
+})
+
+.controller('RasporedNastaveCtrl', function($scope, $ionicSideMenuDelegate) {
+  $ionicSideMenuDelegate.toggleLeft();
+  //Raspored Nastave controller
+})
+
+.controller('KalendarRadaCtrl', function($scope, $ionicSideMenuDelegate) {
+  $ionicSideMenuDelegate.toggleLeft();
+  //Kalendar Rada controller
+})
+
+.controller('KontaktCtrl', function($scope, $ionicSideMenuDelegate) {
+  $ionicSideMenuDelegate.toggleLeft();
+  //Kontakt controller
+})
