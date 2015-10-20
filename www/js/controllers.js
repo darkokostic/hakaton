881f0de94800d18d60016162a603802d00a128cb
vukasinovic <vukasinovic@outlook.com>
2015-10-20 17:27:13 +0200

    Kontakt strana

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,6 +12,21 @@ angular.module('starter.controllers', [])
 })
 
 .controller('PocetnaCtrl', function($scope, $ionicLoading, $http) {
+  $scope.doRefresh = function() {
+    $http.get('http://www.vtsnis.edu.rs/').success(function (data) {
+      //Extract text using jquery
+      $(function(){
+        var $str = $(data);
+        var htmlData = $str.find("td[colspan$='3']").text();
+        var obavestenja = htmlData.replace(/\s\s\s+/g, ' ');
+        $scope.sajt = obavestenja.replace(/О Б А В Е Ш Т Е Њ Е/g , '\n\nО Б А В Е Ш Т Е Њ Е\n\n').replace(/УПОЗОРЕЊЕ/g , '\n\nУПОЗОРЕЊЕ\n\n').replace(/КОНКУРС/g , '\n\nКОНКУРС\n\n');
+      })
+      //END OF Extract
+
+    });
+    $scope.$broadcast('scroll.refreshComplete');
+  }
+
   $scope.show = function() {
     $ionicLoading.show({
       template: 'Учитавање обавештења...',
